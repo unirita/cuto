df655e023897d63ee3b36e835a432d9947bd9398
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-04-23 18:10:06 +0900

    Bug fix to prevent occur panic at servant.Do

--- src/cuto/servant/remote/session.go
+++ src/cuto/servant/remote/session.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"cuto/console"
+	"cuto/db"
 	"cuto/log"
 	"cuto/message"
 	"cuto/servant/config"
@@ -23,7 +24,7 @@ type Session struct {
 	Body string
 
 	endHeartbeatCh chan endSig
-	doRequest      func(req *message.Request, conf *config.ServantConfig, stCh chan<- string) *message.Response
+	doJobRequest   func(req *message.Request, conf *config.ServantConfig, stCh chan<- string) *message.Response
 }
 
 // Sessionオブジェクトのコンストラクタ
@@ -31,7 +32,7 @@ func NewSession(conn net.Conn, body string) *Session {
 	s := new(Session)
 	s.Conn = conn
 	s.Body = body
-	s.doRequest = job.DoJobRequest
+	s.doJobRequest = job.DoJobRequest
 	s.startHeartbeat()
 	return s
 }
@@ -39,39 +40,51 @@ func NewSession(conn net.Conn, body string) *Session {
 // セッションに対応したジョブ実行要求に基いてジョブを実行する。
 // 引数：conf 設定情報
 // 戻り値：なし
-func (s *Session) Do(conf *config.ServantConfig) {
+func (s *Session) Do(conf *config.ServantConfig) error {
 	defer s.Conn.Close()
+	defer s.endHeartbeat()
 
-	var err error
 	req := new(message.Request)
-	err = req.ParseJSON(s.Body)
-	if err != nil {
+	if err := req.ParseJSON(s.Body); err != nil {
 		console.Display("CTS015E", err.Error())
+		return err
 	}
 
-	err = req.ExpandServantVars()
+	res, err := s.doRequest(req, conf)
 	if err != nil {
-		console.Display("CTS015E", err.Error())
+		log.Error(err)
+		res = s.createErrorResponse(req, err)
 	}
-
-	stCh := make(chan string, 1)
-	go s.waitAndSendStartTime(stCh)
-
-	res := s.doRequest(req, conf, stCh)
-
-	s.endHeartbeat()
-	close(stCh)
 
 	var resMsg string
 	resMsg, err = res.GenerateJSON()
 	if err != nil {
 		log.Error(err)
+		return err
 	}
 
 	_, err = s.Conn.Write([]byte(resMsg))
 	if err != nil {
 		log.Error(err)
+		return err
 	}
+
+	return nil
+}
+
+func (s *Session) doRequest(req *message.Request, conf *config.ServantConfig) (*message.Response, error) {
+	err := req.ExpandServantVars()
+	if err != nil {
+		console.Display("CTS015E", err.Error())
+		return nil, err
+	}
+
+	stCh := make(chan string, 1)
+	go s.waitAndSendStartTime(stCh)
+	defer close(stCh)
+
+	res := s.doJobRequest(req, conf, stCh)
+	return res, nil
 }
 
 // ハートビートを開始する。
@@ -106,4 +119,14 @@ func (s *Session) waitAndSendStartTime(stCh <-chan string) {
 	}
 
 	s.Conn.Write([]byte(message.ST_HEADER + st))
+}
+
+// ジョブ実行結果が得られないようなエラーが発生した場合のレスポンスメッセージを生成する。
+func (s *Session) createErrorResponse(req *message.Request, err error) *message.Response {
+	res := new(message.Response)
+	res.NID = req.NID
+	res.JID = req.JID
+	res.Stat = db.ABNORMAL
+	res.Detail = err.Error()
+	return res
 }
