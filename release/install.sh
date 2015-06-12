4abc5c1a1caccada401be8dfe82e6c51ff7af966
yamashita1218 <katsuji_yamashita@unirita.co.jp>
2015-06-12 15:47:29 +0900

    Add silent install mode.

--- release/install.sh
+++ release/install.sh
@@ -36,45 +36,53 @@ if [ -s .installsed ] ; then
 fi
 echo "Ok ...\n"
 
-YES_NO=
-while [ -z "$YES_NO" ] ; do
-    echo "Do you want to install GoCuto v0.1.0 [ y/n ] ?"
-    read YES_NO
-    if [ "$YES_NO" = "y" ] ; then
-        echo "\nStarting to install..."
-    elif [ "$YES_NO" = "n" ] ; then
-        echo "\n...canceled"
-        exit
-    else
-        YES_NO=
-    fi
-done
-
 INSTALL_DIR=$CURRENT_DIR
-
 BIND_ADDRESS=
-echo "\nPlease enter bind-address name of the CUTO Servant"
-echo " [ Defalt  = 0.0.0.0 ]"
-echo "When you use the defalut value , please push an enter key as it is. "
-read BIND_ADDRESS
-if [ -z "$BIND_ADDRESS" ] ; then
-#    BIND_ADDRESS=$HOSTNAME
+LISTEN_PORT=
+SILENT_MODE=
+if [ "$1" == "-s" ] ; then
+    SILENT_MODE="ON"
     BIND_ADDRESS="0.0.0.0"
-    echo "Use defalt [ $BIND_ADDRESS ]"
-else
-    echo "Node name of the CUTO Servant = $BIND_ADDRESS "
+    LISTEN_PORT="2015"
 fi
 
-LISTEN_PORT=
-echo "\nPlease enter port number of the CUTO Servant "
-echo " [ Defalt Port Number = 2015 ]"
-echo "When you use the defalut value , please push an enter key as it is. "
-read LISTEN_PORT
-if [ -z "$LISTEN_PORT" ] ; then
-    LISTEN_PORT="2015"
-    echo "Use defalt [ $LISTEN_PORT ]"
-else
-    echo "Port number of the CUTO Servant [ $LISTEN_PORT ]\n"
+if [ "$SILENT_MODE" != "ON" ] ; then
+    YES_NO=
+    while [ -z "$YES_NO" ] ; do
+        echo "Do you want to install GoCuto v0.1.0 [ y/n ] ?"
+        read YES_NO
+        if [ "$YES_NO" = "y" ] ; then
+            echo "\nStarting to install..."
+        elif [ "$YES_NO" = "n" ] ; then
+            echo "\n...canceled"
+            exit
+        else
+            YES_NO=
+        fi
+    done
+    
+    echo "\nPlease enter bind-address name of the CUTO Servant"
+    echo " [ Defalt  = 0.0.0.0 ]"
+    echo "When you use the defalut value , please push an enter key as it is. "
+    read BIND_ADDRESS
+    if [ -z "$BIND_ADDRESS" ] ; then
+    #    BIND_ADDRESS=$HOSTNAME
+        BIND_ADDRESS="0.0.0.0"
+        echo "Use defalt [ $BIND_ADDRESS ]"
+    else
+        echo "Node name of the CUTO Servant = $BIND_ADDRESS "
+    fi
+    
+    echo "\nPlease enter port number of the CUTO Servant "
+    echo " [ Defalt Port Number = 2015 ]"
+    echo "When you use the defalut value , please push an enter key as it is. "
+    read LISTEN_PORT
+    if [ -z "$LISTEN_PORT" ] ; then
+        LISTEN_PORT="2015"
+        echo "Use defalt [ $LISTEN_PORT ]"
+    else
+        echo "Port number of the CUTO Servant [ $LISTEN_PORT ]\n"
+    fi
 fi
 
 echo "\nInstall GoCUTO with the following setup information."
@@ -84,19 +92,21 @@ echo "  CUTO Servant bind-address       = $BIND_ADDRESS"
 echo "  CUTO Servant Port Number        = $LISTEN_PORT"
 echo "****************************************************\n"
 
-YES_NO=
-while [ -z "$YES_NO" ] ; do
-    echo "OK? [ y/n ] "
-    read YES_NO
-    if [ "$YES_NO" = "y" ] ; then
-        echo "Installing..."
-    elif [ "$YES_NO" = "n" ] ; then
-        echo "...abort"
-        exit
-    else
-        YES_NO=
-    fi
-done
+if [ "$SILENT_MODE" != "ON" ] ; then
+    YES_NO=
+    while [ -z "$YES_NO" ] ; do
+        echo "OK? [ y/n ] "
+        read YES_NO
+        if [ "$YES_NO" = "y" ] ; then
+            echo "Installing..."
+        elif [ "$YES_NO" = "n" ] ; then
+            echo "...abort"
+            exit
+        else
+            YES_NO=
+        fi
+    done
+fi
 
 DIRC=s/@ROOT/`echo $INSTALL_DIR | sed 's/\//\\\\\//g'`/g
 echo $DIRC >> .installsed
