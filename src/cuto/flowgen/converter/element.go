c5c400e8159f92a0e19cb232c0bbd9c6a2bd1d16
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-07-02 16:32:19 +0900

    Fix flowgen element to add ID automatically.

--- src/cuto/flowgen/converter/element.go
+++ src/cuto/flowgen/converter/element.go
@@ -1,15 +1,24 @@
 package converter
 
+import "fmt"
+
 // Element is a node in the flow description.
-// The parallel block (a block which enclosed in bracket) count as one Element.
+// The gateway block (a block which enclosed in bracket) count as one Element.
 type Element interface {
+	ID() string
 	SetNext(Element)
 	Next() Element
 }
 
 // Implementation struct of Element interface.
 type element struct {
+	id   string
 	next Element
+}
+
+// Get the element id.
+func (e *element) ID() string {
+	return e.id
 }
 
 // Get the next element.
@@ -25,35 +34,50 @@ func (e *element) SetNext(n Element) {
 // Job is a job element in the flow description.
 type Job struct {
 	element
-
-	// Job name
-	Name string
+	name string
 }
+
+const jPrefix = "job"
+
+var jIndex int = 1
 
 // Create new Job object with name.
 func NewJob(name string) *Job {
 	j := new(Job)
-	j.Name = name
+	j.id = fmt.Sprintf("%s%d", jPrefix, jIndex)
+	j.name = name
+	jIndex++
 	return j
 }
 
-// Parallel is a block which enclosed in bracket in the flow description.
-// Parallel has paths which consists of jobs.
-type Parallel struct {
+// Get the job name.
+func (j *Job) Name() string {
+	return j.name
+}
+
+const gPrefix = "gw"
+
+var gIndex int = 1
+
+// Gateway is a block which enclosed in bracket in the flow description.
+// Gateway has paths which consists of jobs.
+type Gateway struct {
 	element
 
 	// Head job of paths.
 	PathHeads []*Job
 }
 
-// Create new Parallel object with no path.
-func NewParallel() *Parallel {
-	p := new(Parallel)
-	p.PathHeads = make([]*Job, 0)
-	return p
+// Create new Gateway object with no path.
+func NewGateway() *Gateway {
+	g := new(Gateway)
+	g.id = fmt.Sprintf("%s%d", gPrefix, gIndex)
+	g.PathHeads = make([]*Job, 0)
+	gIndex++
+	return g
 }
 
-// Add path to Parallel.
-func (p *Parallel) AddPathHead(head *Job) {
-	p.PathHeads = append(p.PathHeads, head)
+// Add path to Gateway.
+func (g *Gateway) AddPathHead(head *Job) {
+	g.PathHeads = append(g.PathHeads, head)
 }
--- src/cuto/flowgen/converter/element_test.go
+++ src/cuto/flowgen/converter/element_test.go
@@ -2,103 +2,114 @@ package converter
 
 import "testing"
 
-func TestNewJob(t *testing.T) {
-	j := NewJob("test")
+func initIndexes() {
+	jIndex = 1
+	gIndex = 1
+}
 
-	if j.Name != "test" {
-		t.Errorf("Job name[%s] is wrong.", j.Name)
+func TestNewJob(t *testing.T) {
+	initIndexes()
+	j := NewJob("test")
+	if j.ID() != "job1" {
+		t.Errorf("Job ID[%s] is wrong.", j.ID())
+	}
+	if j.Name() != "test" {
+		t.Errorf("Job name[%s] is wrong.", j.Name())
 	}
 }
 
 func TestJobNext_Job(t *testing.T) {
-	j1 := NewJob("test1")
-	j2 := NewJob("test2")
+	initIndexes()
+	j := NewJob("test1")
 
-	j1.SetNext(j2)
-	if j1.Next() == nil {
+	j.SetNext(NewJob("test2"))
+	if j.Next() == nil {
 		t.Fatal("Failed to set next job.")
 	}
-
-	nextJob, ok := j1.Next().(*Job)
-	if !ok {
-		t.Fatal("Next element is not job.")
+	if j.Next().ID() != "job2" {
+		t.Errorf("Next job's ID[%s] is wrong. Expected [%s]", j.Next().ID(), "job2")
 	}
-	if nextJob.Name != "test2" {
-		t.Errorf("Next job's name[%s] is wrong. Expected [%s]", nextJob.Name, "test2")
+	if _, ok := j.Next().(*Job); !ok {
+		t.Fatal("Next element is not job.")
 	}
 }
 
-func TestJobNext_Parallel(t *testing.T) {
+func TestJobNext_Gateway(t *testing.T) {
+	initIndexes()
 	j := NewJob("test")
-	p := NewParallel()
+	p := NewGateway()
 
 	j.SetNext(p)
 	if j.Next() == nil {
 		t.Fatal("Failed to set next job.")
 	}
-
-	_, ok := j.Next().(*Parallel)
-	if !ok {
-		t.Fatal("Next element is not parallel.")
+	if j.Next().ID() != "gw1" {
+		t.Errorf("Next gateway's ID[%s] is wrong. Expected [%s]", j.Next().ID(), "gw1")
+	}
+	if _, ok := j.Next().(*Gateway); !ok {
+		t.Fatal("Next element is not gateway.")
 	}
 }
 
-func TestNewParallel(t *testing.T) {
-	p := NewParallel()
-
+func TestNewGateway(t *testing.T) {
+	initIndexes()
+	p := NewGateway()
+	if p.ID() != "gw1" {
+		t.Errorf("Gateway ID[%s] is wrong.", p.ID())
+	}
 	if p.PathHeads == nil {
 		t.Fatal("PathHeads is not set.")
 	}
 	if len(p.PathHeads) != 0 {
-		t.Errorf("PathHeads size[%d] must be 0.", len(p.PathHeads))
+		t.Errorf("PathHeads size[%d] must be %d.", len(p.PathHeads), 0)
 	}
 }
 
-func TestParallelNext_Job(t *testing.T) {
-	p := NewParallel()
-	j := NewJob("test1")
+func TestGatewayNext_Job(t *testing.T) {
+	initIndexes()
+	p := NewGateway()
 
-	p.SetNext(j)
+	p.SetNext(NewJob("test"))
 	if p.Next() == nil {
 		t.Fatal("Failed to set next job.")
 	}
-
-	nextJob, ok := p.Next().(*Job)
-	if !ok {
-		t.Fatal("Next element is not job.")
+	if p.Next().ID() != "job1" {
+		t.Errorf("Next job's ID[%s] is wrong. Expected [%s]", p.Next().ID(), "job1")
 	}
-	if nextJob.Name != "test1" {
-		t.Errorf("Next job's name[%s] is wrong. Expected [%s]", nextJob.Name, "test1")
+	if _, ok := p.Next().(*Job); !ok {
+		t.Fatal("Next element is not job.")
 	}
 }
 
-func TestParallelNext_Parallel(t *testing.T) {
-	p1 := NewParallel()
-	p2 := NewParallel()
+func TestGatewayNext_Gateway(t *testing.T) {
+	initIndexes()
+	p := NewGateway()
 
-	p1.SetNext(p2)
-	if p1.Next() == nil {
+	p.SetNext(NewGateway())
+	if p.Next() == nil {
 		t.Fatal("Failed to set next job.")
 	}
-
-	_, ok := p1.Next().(*Parallel)
-	if !ok {
+	if p.Next().ID() != "gw2" {
+		t.Errorf("Next gateway's ID[%s] is wrong. Expected [%s]", p.Next().ID(), "gw2")
+	}
+	if _, ok := p.Next().(*Gateway); !ok {
 		t.Fatal("Next element is not parallel.")
 	}
 }
 
 func TestAddPathHead(t *testing.T) {
-	p := NewParallel()
+	initIndexes()
+	p := NewGateway()
 	p.AddPathHead(NewJob("test1"))
 	p.AddPathHead(NewJob("test2"))
 
 	if len(p.PathHeads) != 2 {
-		t.Fatalf("PathHeads size[%d] must be 2.", len(p.PathHeads))
+		t.Fatalf("PathHeads size[%d] must be %d.", len(p.PathHeads), 2)
 	}
-	if p.PathHeads[0].Name != "test1" {
+	if p.PathHeads[0].ID() != "job1" {
 		t.Errorf("PathHeads[0] name[%s] is wrong. Expected [%s]", p.PathHeads[0], "test1")
 	}
-	if p.PathHeads[1].Name != "test2" {
+	if p.PathHeads[1].ID() != "job2" {
 		t.Errorf("PathHeads[1] name[%s] is wrong. Expected [%s]", p.PathHeads[1], "test2")
 	}
 }
