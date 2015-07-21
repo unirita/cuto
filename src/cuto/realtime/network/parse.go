924484d3f02df11ac1d13cb59e663251bdd7d232
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-07-21 16:49:44 +0900

    Add parse logic for parameter of realtime utility.

---
+++ src/cuto/realtime/network/parse.go
@@ -0,0 +1,55 @@
+package network
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+)
+
+type Network struct {
+	Flow string `json:"flow"`
+	Jobs []Job  `json:"jobs"`
+}
+
+type Job struct {
+	Name    string `json:"name"`
+	Node    string `json:"node"`
+	Port    int    `json:"port"`
+	Path    string `json:"path"`
+	Param   string `json:"param"`
+	Env     string `json:"env"`
+	Work    string `json:"work"`
+	WRC     int    `json:"wrc"`
+	WPtn    string `json:"wptn"`
+	ERC     int    `json:"erc"`
+	EPtn    string `json:"eptn"`
+	Timeout int    `json:"timeout"`
+	SNode   string `json:"snode"`
+	SPort   int    `json:"sport"`
+}
+
+// Parse reads json string from reader, and unmarshal it.
+func Parse(reader io.Reader) (*Network, error) {
+	decorder := json.NewDecoder(reader)
+
+	network := new(Network)
+	if err := decorder.Decode(network); err != nil {
+		return nil, err
+	}
+	if err := network.DetectError(); err != nil {
+		return nil, err
+	}
+
+	return network, nil
+}
+
+// DetectError detects error in Network object, and return it.
+// If there is no error, DetectError returns nil.
+func (n *Network) DetectError() error {
+	for _, job := range n.Jobs {
+		if job.Name == "" {
+			return errors.New("Anonymous job detected.")
+		}
+	}
+	return nil
+}
---
+++ src/cuto/realtime/network/parse_test.go
@@ -0,0 +1,203 @@
+package network
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	jsonStr := `
+{
+	"flow":"job1->job2->[job3,job4->job5]->job6",
+	"jobs":[
+		{
+			"name":"job2",
+			"node":"testnode",
+			"port":1234,
+			"path":"/scripts/job2.sh",
+			"param":"abc",
+			"env":"env1=test",
+			"work":"/work",
+			"wrc":5,
+			"wptn":"warning",
+			"erc":10,
+			"eptn":"error",
+			"timeout":30,
+			"snode":"secondary",
+			"sport":2345
+		},
+		{
+			"name":"job5",
+			"param":"bcd"
+		}
+	]
+}
+`
+	network, err := Parse(strings.NewReader(jsonStr))
+	if err != nil {
+		t.Fatalf("Unexpected error occurd: %s", err)
+	}
+	if network.Flow != "job1->job2->[job3,job4->job5]->job6" {
+		t.Logf("Flow => %s", network.Flow)
+		t.Logf("Want %s", "job1->job2->[job3,job4->job5]->job6")
+		t.Fail()
+	}
+	if len(network.Jobs) != 2 {
+		t.Errorf("len(Jobs) => %d, want %d", len(network.Jobs), 2)
+	}
+
+	firstJob := network.Jobs[0]
+	if firstJob.Name != "job2" {
+		t.Errorf("firstJob.Name => %s, want %s", firstJob.Name, "job2")
+	}
+	if firstJob.Node != "testnode" {
+		t.Errorf("firstJob.Node => %s, want %s", firstJob.Node, "testnode")
+	}
+	if firstJob.Port != 1234 {
+		t.Errorf("firstJob.Port => %d, want %d", firstJob.Port, 1234)
+	}
+	if firstJob.Path != "/scripts/job2.sh" {
+		t.Errorf("firstJob.Path => %s, want %s", firstJob.Path, "/scripts/job2.sh")
+	}
+	if firstJob.Param != "abc" {
+		t.Errorf("firstJob.Param => %s, want %s", firstJob.Param, "abc")
+	}
+	if firstJob.Env != "env1=test" {
+		t.Errorf("firstJob.Env => %s, want %s", firstJob.Env, "env1=test")
+	}
+	if firstJob.Work != "/work" {
+		t.Errorf("firstJob.Work => %s, want %s", firstJob.Work, "/work")
+	}
+	if firstJob.WRC != 5 {
+		t.Errorf("firstJob.WRC => %d, want %d", firstJob.WRC, 5)
+	}
+	if firstJob.WPtn != "warning" {
+		t.Errorf("firstJob.WPtn => %s, want %s", firstJob.WPtn, "warning")
+	}
+	if firstJob.ERC != 10 {
+		t.Errorf("firstJob.ERC => %d, want %d", firstJob.ERC, 10)
+	}
+	if firstJob.EPtn != "error" {
+		t.Errorf("firstJob.EPtn => %s, want %s", firstJob.EPtn, "error")
+	}
+	if firstJob.Timeout != 30 {
+		t.Errorf("firstJob.Timeout => %d, want %d", firstJob.Timeout, 30)
+	}
+	if firstJob.SNode != "secondary" {
+		t.Errorf("firstJob.SNode => %s, want %s", firstJob.SNode, "secondary")
+	}
+	if firstJob.SPort != 2345 {
+		t.Errorf("firstJob.SPort => %d, want %d", firstJob.SPort, 2345)
+	}
+
+	secondJob := network.Jobs[1]
+	if secondJob.Name != "job5" {
+		t.Errorf("secondJob.Name => %s, want %s", secondJob.Name, "job5")
+	}
+	if secondJob.Node != "" {
+		t.Errorf("secondJob.Node => %s, want %s", secondJob.Node, "")
+	}
+	if secondJob.Port != 0 {
+		t.Errorf("secondJob.Port => %d, want %d", secondJob.Port, 0)
+	}
+	if secondJob.Path != "" {
+		t.Errorf("secondJob.Path => %s, want %s", secondJob.Path, "")
+	}
+	if secondJob.Param != "bcd" {
+		t.Errorf("secondJob.Param => %s, want %s", secondJob.Param, "bcd")
+	}
+	if secondJob.Env != "" {
+		t.Errorf("secondJob.Env => %s, want %s", secondJob.Env, "")
+	}
+	if secondJob.Work != "" {
+		t.Errorf("secondJob.Work => %s, want %s", secondJob.Work, "")
+	}
+	if secondJob.WRC != 0 {
+		t.Errorf("secondJob.WRC => %d, want %d", secondJob.WRC, 0)
+	}
+	if secondJob.WPtn != "" {
+		t.Errorf("secondJob.WPtn => %s, want %s", secondJob.WPtn, "")
+	}
+	if secondJob.ERC != 0 {
+		t.Errorf("secondJob.ERC => %d, want %d", secondJob.ERC, 0)
+	}
+	if secondJob.EPtn != "" {
+		t.Errorf("secondJob.EPtn => %s, want %s", secondJob.EPtn, "")
+	}
+	if secondJob.Timeout != 0 {
+		t.Errorf("secondJob.Timeout => %d, want %d", secondJob.Timeout, 0)
+	}
+	if secondJob.SNode != "" {
+		t.Errorf("secondJob.SNode => %s, want %s", secondJob.SNode, "")
+	}
+	if secondJob.SPort != 0 {
+		t.Errorf("secondJob.SPort => %d, want %d", secondJob.SPort, 0)
+	}
+}
+
+func TestParse_WithJSONError(t *testing.T) {
+	jsonStr := `
+{
+	flow:"job1->job2->[job3,job4->job5]->job6",
+	"jobs":[
+		{
+			"name":"job2",
+			"node":"testnode",
+			"port":1234,
+			"path":"/scripts/job2.sh",
+			"param":"abc",
+			"env":"env1=test",
+			"work":"/work",
+			"wrc":5,
+			"wptn":"warning",
+			"erc":10,
+			"eptn":"error",
+			"timeout":30,
+			"snode":"secondary",
+			"sport":2345
+		},
+		{
+			"name":"job5",
+			"param":"bcd"
+		}
+	]
+}
+`
+	_, err := Parse(strings.NewReader(jsonStr))
+	if err == nil {
+		t.Fatalf("No error occured.")
+	}
+}
+
+func TestParse_WithAnonymousJob(t *testing.T) {
+	jsonStr := `
+{
+	"flow":"job1->job2->[job3,job4->job5]->job6",
+	"jobs":[
+		{
+			"node":"testnode",
+			"port":1234,
+			"path":"/scripts/job2.sh",
+			"param":"abc",
+			"env":"env1=test",
+			"work":"/work",
+			"wrc":5,
+			"wptn":"warning",
+			"erc":10,
+			"eptn":"error",
+			"timeout":30,
+			"snode":"secondary",
+			"sport":2345
+		},
+		{
+			"name":"job5",
+			"param":"bcd"
+		}
+	]
+}
+`
+	_, err := Parse(strings.NewReader(jsonStr))
+	if err == nil {
+		t.Fatalf("No error occured.")
+	}
+}
