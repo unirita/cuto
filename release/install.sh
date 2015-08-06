c46b0a2ac123603af6ff1545114e4ec46cc5b833
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-08-06 13:51:20 +0900

    Change version to V0.9.4

--- release/install.sh
+++ release/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VERSION="V1.0.3L0"
+VERSION="V0.9.4L0"
 
 echo -e "\n"
 echo "***********************************************************"
 echo "*                                                         *"
 echo "*                                                         *"
 echo "*           GoCUTO $VERSION Instaler                      *"
-echo "*                         Last Update 2015/07/29          *"
+echo "*                         Last Update 2015/08/06          *"
 echo "*                                           UNIRITA Inc.  *"
 echo "*                                                         *"
 echo "***********************************************************"
--- src/cuto/master/version.go
+++ src/cuto/master/version.go
@@ -4,4 +4,4 @@
 package main
 
 // マスタのバージョン情報
-const Version = "1.0.3.0"
+const Version = "0.9.4.0"
