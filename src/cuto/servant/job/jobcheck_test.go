16a9b75c7eefb7322f4a02ccad732e6379b40282
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-09-01 11:53:46 +0900

    Add test case.

--- src/cuto/servant/job/jobcheck_test.go
+++ src/cuto/servant/job/jobcheck_test.go
@@ -47,7 +47,7 @@ func TestDoJobResultCheck_Base(t *testing.T) {
 		t.Errorf("result.RC => %d, wants %d", result.RC, 5)
 	}
 	if result.Stat != 2 {
-		t.Errorf("result.Stat => %d, wants %d", result.Stat, 5)
+		t.Errorf("result.Stat => %d, wants %d", result.Stat, 2)
 	}
 	if result.Var != "testvar" {
 		t.Errorf("result.Var => %s, wants %s", result.Var, "testvar")
@@ -80,6 +80,41 @@ func TestDoJobResultCheck_RegardNoRecordJobAsExecuting(t *testing.T) {
 	}
 	if result.Stat != 0 {
 		t.Errorf("result.Stat => %d, wants %d", result.Stat, 0)
+	}
+}
+
+func TestDoJobResultCheck_DifferentNID(t *testing.T) {
+	chk := &message.JobCheck{
+		Type:    "jobcheck",
+		Version: "1.2.3",
+		NID:     2,
+		JID:     "job1",
+	}
+
+	result := DoJobResultCheck(chk, getJobCheckTestConfig())
+	if result == nil {
+		t.Fatalf("DoJobResultCheck() returned nil.")
+	}
+	if result.NID != 2 {
+		t.Errorf("result.NID => %d, wants %d", result.NID, 1)
+	}
+	if result.JID != "job1" {
+		t.Errorf("result.JID => %s, wants %s", result.JID, "job1")
+	}
+	if result.RC != 15 {
+		t.Errorf("result.RC => %d, wants %d", result.RC, 15)
+	}
+	if result.Stat != 9 {
+		t.Errorf("result.Stat => %d, wants %d", result.Stat, 9)
+	}
+	if result.Var != "error" {
+		t.Errorf("result.Var => %s, wants %s", result.Var, "error")
+	}
+	if result.St != "2015-07-31 14:45:56.321" {
+		t.Errorf("result.St => %s, wants %s", result.St, "2015-07-31 14:45:56.321")
+	}
+	if result.Et != "2015-07-31 15:12:34.567" {
+		t.Errorf("result.Et => %s, wants %s", result.Et, "2015-07-31 15:12:34.567")
 	}
 }
 
