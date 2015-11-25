839e843832b65a89e328869f0a9f4466764d362a
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-11-25 11:49:39 +0900

    Refactoring.

--- servant/job/job_unix_test.go
+++ servant/job/job_unix_test.go
@@ -1,64 +1,21 @@
 // Copyright 2015 unirita Inc.
 // Created 2015/04/10 shanxia
 
+// +build darwin linux
+
 package job
 
 import (
 	"bufio"
-	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/unirita/cuto/db"
 	"github.com/unirita/cuto/message"
-	"github.com/unirita/cuto/servant/config"
-	"github.com/unirita/cuto/testutil"
 )
 
-var conf *config.ServantConfig
-var testJobPath string
-
-// ジョブログなどの掃除
-func init() {
-	time.Local = time.FixedZone("JST", 9*60*60)
-
-	testJobPath = filepath.Join(testutil.GetBaseDir(), "servant", "job", "_testdata")
-	err := os.Chdir(testJobPath)
-	config.RootPath = testJobPath
-	if err != nil {
-		panic(err.Error())
-	}
-	configPath := filepath.Join(testJobPath, "servant_l.ini")
-	conf = config.ReadConfig(configPath)
-	os.RemoveAll(conf.Dir.JoblogDir)
-	err = os.Mkdir(conf.Dir.JoblogDir, 0755)
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
-// メッセージ・DB向けの時刻フォーマットをジョブログファイル名向けのフォーマットに変換する。
-func stToLocalTimestamp(st string) string {
-	t, err := time.ParseInLocation("2006-01-02 15:04:05.000", st, time.UTC)
-	if err != nil {
-		panic("Unexpected time format: " + err.Error())
-	}
-	return t.Local().Format("20060102150405.000")
-}
-
-// ジョブログファイル名をフルパスで作成する。
-// ”開始日(YYYYMMDD)\インスタンスID.ジョブ名（拡張子なし）.開始日時（yyyyMMddHHmmss.sss).log
-func createJoblogFileName(req *message.Request, st string, nID int, jID string) string {
-	job := filepath.Base(req.Path)
-	if extpos := strings.LastIndex(job, "."); extpos != -1 {
-		job = job[:extpos]
-	}
-	joblogFile := fmt.Sprintf("%v.%v.%v.%v.log", nID, job, jID, st)
-	return filepath.Join(conf.Dir.JoblogDir, st[:8], joblogFile)
-}
+const testServantIni = "servant_l.ini"
 
 func TestDoJobRequest_拡張子無しSHジョブが正常に実行できる(t *testing.T) {
 	req := &message.Request{
--- servant/job/job_windows_test.go
+++ servant/job/job_windows_test.go
@@ -5,59 +5,15 @@ package job
 
 import (
 	"bufio"
-	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/unirita/cuto/db"
 	"github.com/unirita/cuto/message"
-	"github.com/unirita/cuto/servant/config"
-	"github.com/unirita/cuto/testutil"
 )
 
-var conf *config.ServantConfig
-
-// ジョブログなどの掃除
-func init() {
-	time.Local = time.FixedZone("JST", 9*60*60)
-
-	testPath := filepath.Join(testutil.GetBaseDir(), "servant", "job", "_testdata")
-	err := os.Chdir(testPath)
-	config.RootPath = testPath
-	if err != nil {
-		panic(err.Error())
-	}
-	configPath := filepath.Join(testPath, "servant.ini")
-	conf = config.ReadConfig(configPath)
-	os.RemoveAll(conf.Dir.JoblogDir)
-	err = os.Mkdir(conf.Dir.JoblogDir, 0666)
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
-// メッセージ・DB向けの時刻フォーマットをジョブログファイル名向けのフォーマットに変換する。
-func stToLocalTimestamp(st string) string {
-	t, err := time.ParseInLocation("2006-01-02 15:04:05.000", st, time.UTC)
-	if err != nil {
-		panic("Unexpected time format: " + err.Error())
-	}
-	return t.Local().Format("20060102150405.000")
-}
-
-// ジョブログファイル名をフルパスで作成する。
-// ”開始日(YYYYMMDD)\インスタンスID.ジョブ名（拡張子なし）.開始日時（yyyyMMddHHmmss.sss).log
-func createJoblogFileName(req *message.Request, st string, nID int, jID string) string {
-	job := filepath.Base(req.Path)
-	if extpos := strings.LastIndex(job, "."); extpos != -1 {
-		job = job[:extpos]
-	}
-	joblogFile := fmt.Sprintf("%v.%v.%v.%v.log", nID, job, jID, st)
-	return filepath.Join(conf.Dir.JoblogDir, st[:8], joblogFile)
-}
+const testServantIni = "servant.ini"
 
 func TestDoJobRequest_ジョブが正常に実行できる(t *testing.T) {
 	req := &message.Request{
