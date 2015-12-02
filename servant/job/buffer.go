1a7bc6e96035ab1ae117e7a70c38c8d27d0efb85
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-12-02 10:28:14 +0900

    Fix servant to remove unnecessary channel.

--- servant/job/buffer.go
+++ servant/job/buffer.go
@@ -31,25 +31,20 @@ func NewOutputPipeBuffer(enableOutput bool) *OutputPipeBuffer {
 	return &OutputPipeBuffer{enableOutput: enableOutput}
 }
 
-func (b *OutputPipeBuffer) ReadPipe(pStdout io.ReadCloser, pStderr io.ReadCloser, endSig <-chan struct{}) error {
+func (b *OutputPipeBuffer) ReadPipe(pStdout io.ReadCloser, pStderr io.ReadCloser) error {
 	reader := io.MultiReader(pStdout, pStderr)
 	buf := make([]byte, 1024)
 	for {
-		select {
-		case <-endSig:
+		n, err := reader.Read(buf)
+		if n == 0 {
 			return nil
-		default:
-			n, err := reader.Read(buf)
-			if n == 0 {
-				return nil
-			}
-			if err != nil {
-				return err
-			}
-			if b.enableOutput {
-				os.Stdout.Write(buf[:n])
-			}
-			b.Buffer.Write(buf[:n])
 		}
+		if err != nil {
+			return err
+		}
+		if b.enableOutput {
+			os.Stdout.Write(buf[:n])
+		}
+		b.Buffer.Write(buf[:n])
 	}
 }
